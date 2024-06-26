ffb7680d57ef353f711931d791052c0ffa665d77
goncalves <goncalvesdacosta05@gmail.com>
2024-06-26 20:03:39 -0300

    refactored classes and deleted specific sports class

--- classes/equipe/Equipe.java
+++ classes/equipe/Equipe.java
@@ -1,56 +1,58 @@
+package classes.equipe;
+
 import java.util.ArrayList;
 import classes.usuario.Usuario;
 import classes.esportes.Esportes;
 
 public class Equipe {
-    private String nome;
-    private ArrayList<Usuario> jogadores;
-    private Usuario capitao;
-    private int pontuacao;
-    private Esportes esporte;
+  private String nome;
+  private ArrayList<Usuario> jogadores;
+  private Usuario capitao;
+  private int pontuacao;
+  private Esportes esporte;
 
-    public Equipe(String nome, Usuario capitao, Esportes esporte) {
-        validarParametros(nome, capitao, esporte);
-        this.nome = nome;
-        this.capitao = capitao;
-        this.esporte = esporte;
-        this.jogadores = new ArrayList<>();
-        this.jogadores.add(capitao);
-        this.pontuacao = 0;
-    }
+  public Equipe(String nome, Usuario capitao, Esportes esporte) {
+    validarParametros(nome, capitao, esporte);
+    this.nome = nome;
+    this.capitao = capitao;
+    this.esporte = esporte;
+    this.jogadores = new ArrayList<>();
+    this.jogadores.add(capitao);
+    this.pontuacao = 0;
+  }
 
-    private void validarParametros(String nome, Usuario capitao, Esportes esporte) {
-        if (nome == null || capitao == null || esporte == null) {
-            throw new IllegalArgumentException("Nome, capitão e esporte não podem ser nulos.");
-        }
+  private void validarParametros(String nome, Usuario capitao, Esportes esporte) {
+    if (nome == null || capitao == null || esporte == null) {
+      throw new IllegalArgumentException("Nome, capitão e esporte não podem ser nulos.");
     }
+  }
 
-    public void adicionarJogador(Usuario jogador) {
-        if (!jogadores.contains(jogador)) {
-            jogadores.add(jogador);
-        }
+  public void adicionarJogador(Usuario jogador) {
+    if (!jogadores.contains(jogador)) {
+      jogadores.add(jogador);
     }
+  }
 
-    public void removerJogador(Usuario jogador) {
-        if (jogador != capitao) {
-            jogadores.remove(jogador);
-        }
+  public void removerJogador(Usuario jogador) {
+    if (jogador != capitao) {
+      jogadores.remove(jogador);
     }
+  }
 
-    public void adicionarPontuacao(int pontos) {
-        this.pontuacao += pontos;
-    }
+  public void adicionarPontuacao(int pontos) {
+    this.pontuacao += pontos;
+  }
 
-    public void diminuirPontuacao(int pontos) {
-        this.pontuacao -= pontos;
-    }
+  public void diminuirPontuacao(int pontos) {
+    this.pontuacao -= pontos;
+  }
 
-    public boolean estaCompleto(int maxJogadores) {
-        return jogadores.size() == maxJogadores;
-    }
+  public boolean estaCompleto(int maxJogadores) {
+    return jogadores.size() == maxJogadores;
+  }
 
-    public int getNumeroJogadores() {
-        return jogadores.size();
-    }
+  public int getNumeroJogadores() {
+    return jogadores.size();
+  }
 
 }
--- classes/esportes/Esportes.java
+++ classes/esportes/Esportes.java
@@ -8,14 +8,12 @@ public class Esportes {
     private int quantidadeJogadores;
     private ArrayList<Quadra> quadras;
 
-    // Construtor
     public Esportes(String nome, int quantidadeJogadores) {
         this.nome = nome;
         this.quantidadeJogadores = quantidadeJogadores;
         this.quadras = new ArrayList<>();
     }
 
-    // Getters e Setters
     public String getNome() {
         return nome;
     }
@@ -40,12 +38,10 @@ public class Esportes {
         this.quadras = quadras;
     }
 
-    // Método para adicionar uma quadra
     public void adicionarQuadra(Quadra quadra) {
         this.quadras.add(quadra);
     }
 
-    // Método para mostrar as quadras
     public void mostrarQuadras() {
         System.out.println("Quadras disponíveis para " + this.nome + ":");
         for (int i = 0; i < quadras.size(); i++) {
@@ -57,51 +53,51 @@ public class Esportes {
 // import java.util.ArrayList;
 
 // public class Esportes {
-//     private String nome;
-//     private int quantidadeJogadores;
-//     private ArrayList<Quadra> quadras;
+// private String nome;
+// private int quantidadeJogadores;
+// private ArrayList<Quadra> quadras;
 
-//     public Esportes(String nome, int quantidadeJogadores) {
-//         this.nome = nome;
-//         this.quantidadeJogadores = quantidadeJogadores;
-//         this.quadras = new ArrayList<>();
-//     }
+// public Esportes(String nome, int quantidadeJogadores) {
+// this.nome = nome;
+// this.quantidadeJogadores = quantidadeJogadores;
+// this.quadras = new ArrayList<>();
+// }
 
-//     public void setQuantidadeJogadores(int quantidadeJogadores) {
-//         this.quantidadeJogadores = quantidadeJogadores;
-//     }
+// public void setQuantidadeJogadores(int quantidadeJogadores) {
+// this.quantidadeJogadores = quantidadeJogadores;
+// }
 
-//     public ArrayList<Quadra> getQuadras() {
-//         return quadras;
-//     }
+// public ArrayList<Quadra> getQuadras() {
+// return quadras;
+// }
 
-//     public void setQuadras(ArrayList<Quadra> quadras) {
-//         this.quadras = quadras;
-//     }
+// public void setQuadras(ArrayList<Quadra> quadras) {
+// this.quadras = quadras;
+// }
 
-//     public void adicionarQuadra(Quadra quadra) {
-//         if (!quadras.contains(quadra)) {
-//             quadras.add(quadra);
-//         }
-//     }
+// public void adicionarQuadra(Quadra quadra) {
+// if (!quadras.contains(quadra)) {
+// quadras.add(quadra);
+// }
+// }
 
-//     public void mostrarQuadras() {
-//         if (quadras.isEmpty()) {
-//             System.out.println("Não há quadras disponíveis para " + nome + ".");
-//         } else {
-//             System.out.println("Quadras disponíveis para " + nome + ":");
-//             for (int i = 0; i < quadras.size(); i++) {
-//                 System.out.println((i + 1) + ". " + quadras.get(i));
-//             }
-//         }
-//     }
+// public void mostrarQuadras() {
+// if (quadras.isEmpty()) {
+// System.out.println("Não há quadras disponíveis para " + nome + ".");
+// } else {
+// System.out.println("Quadras disponíveis para " + nome + ":");
+// for (int i = 0; i < quadras.size(); i++) {
+// System.out.println((i + 1) + ". " + quadras.get(i));
+// }
+// }
+// }
 
-//     // Getters and Setters for nome
-//     public String getNome() {
-//         return nome;
-//     }
+// // Getters and Setters for nome
+// public String getNome() {
+// return nome;
+// }
 
-//     public void setNome(String nome) {
-//         this.nome = nome;
-//     }
+// public void setNome(String nome) {
+// this.nome = nome;
+// }
 // }

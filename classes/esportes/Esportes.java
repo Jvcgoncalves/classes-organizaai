5f17e11feea1a1d7fae7dd2eeeb8ca96a7cb4974
Luiz Colman <luiz.colman@alfaconcursos.com.br>
2024-06-26 19:53:28 -0300

    added Esportes class

--- classes/esportes/Esportes.java
+++ classes/esportes/Esportes.java
@@ -1,39 +1,107 @@
 package classes.esportes;
 
+import java.util.ArrayList;
 import classes.quadra.Quadra;
 
-import java.util.ArrayList;
-
 public class Esportes {
-    protected ArrayList<Quadra> quadras;
+    private String nome;
+    private int quantidadeJogadores;
+    private ArrayList<Quadra> quadras;
 
-    public Esportes() {
+    // Construtor
+    public Esportes(String nome, int quantidadeJogadores) {
+        this.nome = nome;
+        this.quantidadeJogadores = quantidadeJogadores;
         this.quadras = new ArrayList<>();
     }
 
-    public void mostrarQuadras() {
-        for (Quadra quadra : quadras) {
-            quadra.verDetalhes();
-        }
+    // Getters e Setters
+    public String getNome() {
+        return nome;
     }
 
-    public void setQuadras(ArrayList<Quadra> quadras) {
-        this.quadras = quadras;
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeJogadores() {
+        return quantidadeJogadores;
+    }
+
+    public void setQuantidadeJogadores(int quantidadeJogadores) {
+        this.quantidadeJogadores = quantidadeJogadores;
     }
 
     public ArrayList<Quadra> getQuadras() {
         return quadras;
     }
 
-    public void addQuadra(Quadra quadra) {
-        quadras.add(quadra);
+    public void setQuadras(ArrayList<Quadra> quadras) {
+        this.quadras = quadras;
     }
 
-    public void removerQuadra(String quadraId) {
-        this.quadras.removeIf(quadra -> quadra.getId() == quadraId);
+    // Método para adicionar uma quadra
+    public void adicionarQuadra(Quadra quadra) {
+        this.quadras.add(quadra);
     }
 
-    public int getNumeroMinimoJogadores() {
-
+    // Método para mostrar as quadras
+    public void mostrarQuadras() {
+        System.out.println("Quadras disponíveis para " + this.nome + ":");
+        for (int i = 0; i < quadras.size(); i++) {
+            System.out.println((i + 1) + ". " + quadras.get(i).toString());
+        }
     }
 }
+
+// import java.util.ArrayList;
+
+// public class Esportes {
+//     private String nome;
+//     private int quantidadeJogadores;
+//     private ArrayList<Quadra> quadras;
+
+//     public Esportes(String nome, int quantidadeJogadores) {
+//         this.nome = nome;
+//         this.quantidadeJogadores = quantidadeJogadores;
+//         this.quadras = new ArrayList<>();
+//     }
+
+//     public void setQuantidadeJogadores(int quantidadeJogadores) {
+//         this.quantidadeJogadores = quantidadeJogadores;
+//     }
+
+//     public ArrayList<Quadra> getQuadras() {
+//         return quadras;
+//     }
+
+//     public void setQuadras(ArrayList<Quadra> quadras) {
+//         this.quadras = quadras;
+//     }
+
+//     public void adicionarQuadra(Quadra quadra) {
+//         if (!quadras.contains(quadra)) {
+//             quadras.add(quadra);
+//         }
+//     }
+
+//     public void mostrarQuadras() {
+//         if (quadras.isEmpty()) {
+//             System.out.println("Não há quadras disponíveis para " + nome + ".");
+//         } else {
+//             System.out.println("Quadras disponíveis para " + nome + ":");
+//             for (int i = 0; i < quadras.size(); i++) {
+//                 System.out.println((i + 1) + ". " + quadras.get(i));
+//             }
+//         }
+//     }
+
+//     // Getters and Setters for nome
+//     public String getNome() {
+//         return nome;
+//     }
+
+//     public void setNome(String nome) {
+//         this.nome = nome;
+//     }
+// }
